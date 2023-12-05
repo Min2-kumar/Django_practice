ad50b64959cebc0f4ec39214565b825c1e529342
Min2-Kumar <mkreborn2@gmail.com>
2023-12-05 15:53:02 +0530

    Creating part of CRUD operations (C)

--- django_orm/views.py
+++ django_orm/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Student
 
 # Create your views here.
+
+def index(request):
+    return render(request,'django_orm/index.html')
+
+
+def submit(request):
+    data = request.POST
+    if request.method == "POST":
+        name = data['name']
+        age = data['age']
+        email = data['email']
+        marks = data['marks']
+        Student.objects.create(stuName=name, stuAge=age, stuEmail = email, stuMarks = marks)
+    return render(request,'django_orm/index.html')
--- django_practice/settings.py
+++ django_practice/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'django_orm'
 ]
 
@@ -55,7 +56,6 @@ ROOT_URLCONF = 'django_practice.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
