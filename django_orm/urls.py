a9f45b0c34b717cf36f78bd072b5b478a1d12853
Prakash Kumar <prakash@grocurv.com>
2023-12-07 15:59:11 +0530

    CRUD done - (RUD)

--- django_orm/urls.py
+++ django_orm/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('submit/', views.submit),
+    path('delete/<int:stu_id>', views.delete, name='delete_student'),
 ]
--- django_orm/views.py
+++ django_orm/views.py
@@ -1,18 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Student
 
 # Create your views here.
 
+# def index(request):
+#     return render(request,'django_orm/index.html')
+
+
 def index(request):
-    return render(request,'django_orm/index.html')
+    id = request.GET.get('id')
+    if id:
+        student = Student.objects.get(id=id)
+    return render(request,'django_orm/index.html', {'students': Student.objects.all(), 'student': student if id else None})
+
+
+# def submit(request):
+#     data = request.POST
+#     if request.method == "POST":
+#         name = data['name']
+#         age = data['age']
+#         email = data['email']
+#         marks = data['marks']
+#         Student.objects.create(stuName=name, stuAge=age, stuEmail = email, stuMarks = marks)
+#     return render(request,'django_orm/index.html')
 
 
 def submit(request):
     data = request.POST
-    if request.method == "POST":
-        name = data['name']
-        age = data['age']
-        email = data['email']
-        marks = data['marks']
-        Student.objects.create(stuName=name, stuAge=age, stuEmail = email, stuMarks = marks)
-    return render(request,'django_orm/index.html')
+    id = data.get('id')
+    if id:
+        s = Student.objects.get(id=id)
+    else:
+        s = Student()
+    s.stuName=data.get('name')
+    s.stuAge=data.get('age')
+    s.stuEmail = data.get('email')
+    s.stuMarks = data.get('marks')
+    s.save()
+    return render(request,'django_orm/thanks.html', {'students': Student.objects.all()})
+
+
+def delete(request, stu_id):
+    Student.objects.filter(id=stu_id).delete()
+    return redirect('/')
